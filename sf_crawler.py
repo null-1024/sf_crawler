35460a41e7a9fa0a9741cc9aae4b94a639f31fe8
nullptr <101915990+null-1024@users.noreply.github.com>
2022-03-21 12:46:08 +0800

    Update sf_crawler.py
    修正 无文字章节误判定成vip章节 的错误

--- sf_crawler.py
+++ sf_crawler.py
@@ -16,10 +16,6 @@ def GetOneBook(url):
     volume_list=etree.HTML(directory).xpath('/html/body/div[1]/div[3]/div[@class="story-catalog"]')
     # 2. 每卷操作
     for volume in volume_list:
-        print(volume_list[0])
-        print(volume)
-        if volume==volume_list[0]:
-            continue
         volumename=volume.xpath('./div[@class="catalog-hd"]/h3/text()')[0]
         print(volumename)
         with open(path+volumename+".md","a",encoding='utf-8') as file:# 解决编码问题(有些文章中有Unicode表情) 解决方案链接 : https://www.cnblogs.com/themost/p/6603409.html
@@ -31,12 +27,11 @@ def GetOneBook(url):
                 chapter_url=chapter.xpath('./a/@href')[0]
                 chapter_html=requests.get("https://book.sfacg.com/"+chapter_url,headers=headers)
                 chapter_content=etree.HTML(chapter_html.text).xpath('/html/body/div[1]/div[3]/div/div/div[2]/div[2]/p/text()')
-                if 0 ==len(chapter_content):# 判断是否是vip章节
+                if chapter_url[0:4]=='/vip':# 判断是否是vip章节
                     return
                 file.write(chaptername+'\n')
                 for paragraph in chapter_content:
                     file.write('\t\t'+paragraph+'\n')
-                    print(paragraph)
                 file.write("\n\n\n")
                 print("over!")
                 # time.sleep(random.randint(1,10)+random.random())
@@ -48,7 +43,10 @@ def NewFolder(name):# 新建文件夹;return 文件夹路径
     return name+"\\"
 
 headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"}
-for id in range(300000,500000):
+for id in range(61423,61424):
     url=f"https://book.sfacg.com/Novel/{id}/MainIndex/"
-    GetOneBook(url)
+    try:
+        GetOneBook(url)
+    except:
+        continue
     print(id)
